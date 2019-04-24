70af3d9ce0f596803b77899f3e645219f559e64a
mahedonas <marcelohenrique1226@outlook.com>
2019-04-24 20:23:30 -0300

    coloquei o modificador de acesso nos atributos

--- src/Carro.java
+++ src/Carro.java
@@ -1,16 +1,23 @@
- 
+
 public class Carro {
- String nome ;
- String marca;
- String cor ;
- String fabricacao ;
- String largura;
- String peso;
 
-public void acelerar(){
-     System.out.println("Carro acelerar!");
-     
+    private String nome;
+    private String marca;
+    private String cor;
+    private String fabricacao;
+    private String largura;
+    private String peso;
 
+    public void acelerar() {
+        System.out.println("Carro acelerar!");
+    }
 
-}
+    public void setNome(String nome) {
+        if (nome != "") {
+            this.nome = nome;
+        }
+    }
+    
+    
+
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,39 +1,39 @@
 
 import java.util.Scanner;
 
-
 public class Principal {
 
     public static void main(String[] args) {
 
         Carro carro01 = new Carro();
         Carro carro02 = new Carro();
-        
-        Scanner scan = new Scanner (System.in);
-        
-        System.out.print(" nome "); 
-        carro01.nome = scan.next();
-        
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print(" nome ");
+        carro01.setNome(scan.next());
+
         System.out.print(" marca ");
         carro01.marca = scan.next();
-        
+
         System.out.print(" cor ");
         carro01.cor = scan.next();
-        
+
         System.out.print("fabricacao");
-        carro01.fabricacao = scan.next ();
-        
-        System.out.print ("lagura");
-        carro01.largura = scan.next ();
-        
-        System.out.print ("peso");
-        carro01.largura = scan.next ();
-       
-        
-       
+        carro01.fabricacao = scan.next();
+
+        System.out.print("lagura");
+        carro01.largura = scan.next();
+
+        System.out.print("peso");
+        carro01.largura = scan.next();
+
         System.out.println("Nome: " + carro01.nome);
         System.out.println("Marca: " + carro02.marca);
-    }   System.out.println("Cor: " + carro01.cor);
+        System.out.println("Cor: " + carro01.cor);
         System.out.println("Fabricaçao: " + carro02.fabricação);
-}       System.out.println("largura: " + carro01.largura);
+        System.out.println("largura: " + carro01.largura);
         System.out.println("peso: " + carro02.peso);
+
+    }
+}
